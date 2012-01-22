2838e99f0ebc3cd61b04c7a04f2793d5f176334f
waleedhassan <waleedot2003@gmail.com>
2012-01-22 15:25:29 +0300

    adding first game gui

--- src/org/gnome/gbrainy/GbrainyActivity.java
+++ src/org/gnome/gbrainy/GbrainyActivity.java
@@ -1,13 +1,36 @@
+/*
+ * Copyright (C) 2007-2011 Jordi Mas i Hernàndez <jmas@softcatala.org>
+ * Copyright (C) 2012-2017 Waleed hassan <waleedhassan2000@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 package org.gnome.gbrainy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GbrainyActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);        
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
     }
 }
